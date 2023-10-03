2c630f97d5c715583d452c8a2a5f75437fffe760
EmilSantos <Santos.N.Emil@gmail.com>
2023-10-03 14:25:03 -0700

    started product screen and removed href tags with proper React Link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
@@ -7,12 +8,20 @@ function Header() {
       <header>
       <Navbar expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">eShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>eShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>cart</Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user"></i>login</Nav.Link>
+              
+            <LinkContainer to='/cart'>
+              <Nav.Link><i className="fas fa-shopping-cart"></i>cart</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to='/login'>
+              <Nav.Link><i className="fas fa-user"></i>login</Nav.Link>
+            </LinkContainer>
 
             </Nav>
           </Navbar.Collapse>
